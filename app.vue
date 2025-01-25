a578e6b310c8c383fa02e4c862f14fe7f8de0312
Sagiv <sagivb13@gmail.com>
2025-01-25 21:22:51 +0200

    set header image

--- app.vue
+++ app.vue
@@ -1,19 +1,12 @@
 <template>
-  <v-responsive class="border rounded" max-height="300">
-    <v-app>
-      <v-app-bar title="App bar"></v-app-bar>
+  <v-app>
+    <v-app-bar title="App bar"></v-app-bar>
 
-      <v-navigation-drawer>
-        <v-list>
-          <v-list-item title="Navigation drawer"></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-
-      <v-main>
-        <v-container>
-          <h1>Main Content</h1>
-        </v-container>
-      </v-main>
-    </v-app>
-  </v-responsive>
+    <v-main>
+      <v-container fluid min-width="100vw">
+        <header-image />
+        <h1>Main Content</h1>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
---
+++ components/headerImage.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-parallax src="/header-image.png" max-height="80vh">
+    <div
+      class="d-flex flex-column fill-height justify-center align-center text-white"
+    >
+      <h1 class="text-h4 font-weight-thin mb-4">משה ליבוביץ</h1>
+      <h4 class="subheading">מאמן? מטפל?</h4>
+    </div>
+  </v-parallax>
+</template>
