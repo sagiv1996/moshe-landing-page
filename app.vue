3a329d54776e3ea266836c697b21f5020d4c7d00
Sagiv <sagivb13@gmail.com>
2025-01-26 08:47:46 +0200

    remove header-image from container, and remove app-bar component

--- app.vue
+++ app.vue
@@ -1,11 +1,9 @@
 <template>
   <v-app>
-    <v-app-bar title="App bar"></v-app-bar>
-
     <v-main>
-      <v-container fluid min-width="100vw">
-        <header-image />
-        <h1>Main Content</h1>
+      <header-image />
+      <v-container>
+        <about-me />
       </v-container>
     </v-main>
   </v-app>
