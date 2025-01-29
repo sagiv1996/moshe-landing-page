14f053892dfa146ac974bf9dd1072b8a430b8710
Sagiv <sagivb13@gmail.com>
2025-01-29 16:30:02 +0200

    add button for google forms

--- app.vue
+++ app.vue
@@ -7,6 +7,15 @@
       <feature-cards />
       <faq />
       <v-fab
+        text="צור קשר עוד היום"
+        prepend-icon="mdi-form-select"
+        app
+        color="info"
+        aria-label="Button of google form"
+        href="https://docs.google.com/forms/d/e/1FAIpQLSf0HIU86b6gVI29v2fZb2VBrvSHVBNJ5bMo1-0vsUY7s71GXw/viewform?embedded=true"
+        target="_blank"
+      />
+      <v-fab
         icon="mdi-whatsapp"
         app
         color="success"
@@ -14,6 +23,7 @@
       whatsapp"
         href="https://wa.me/972535267163?text=%D7%A9%D7%9C%D7%95%D7%9D,%20%D7%94%D7%92%D7%A2%D7%AA%D7%99%20%D7%93%D7%A8%D7%9A%20%D7%94%D7%90%D7%AA%D7%A8%20%D7%95%D7%90%D7%A9%D7%9E%D7%97%20%D7%9C%D7%91%D7%A8%D7%A8%20%D7%A4%D7%A8%D7%98%D7%99%D7%9D."
         target="_blank"
+        class="mb-10"
       />
     </v-main>
   </v-app>
