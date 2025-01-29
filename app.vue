e1c4674b5a66353abb5aed48407e54618e7a1a8c
Sagiv <sagivb13@gmail.com>
2025-01-29 16:07:08 +0200

    add fab icon

--- app.vue
+++ app.vue
@@ -6,6 +6,15 @@
       <global-info />
       <feature-cards />
       <faq />
+      <v-fab
+        icon="mdi-whatsapp"
+        app
+        color="success"
+        aria-label="Button of
+      whatsapp"
+        href="https://wa.me/972535267163?text=%D7%A9%D7%9C%D7%95%D7%9D,%20%D7%94%D7%92%D7%A2%D7%AA%D7%99%20%D7%93%D7%A8%D7%9A%20%D7%94%D7%90%D7%AA%D7%A8%20%D7%95%D7%90%D7%A9%D7%9E%D7%97%20%D7%9C%D7%91%D7%A8%D7%A8%20%D7%A4%D7%A8%D7%98%D7%99%D7%9D."
+        target="_blank"
+      />
     </v-main>
   </v-app>
 </template>
