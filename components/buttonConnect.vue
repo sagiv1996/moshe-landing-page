1558211ebeaf1d3cf639a822301e6e8a3dd026ae
Sagiv <sagivb13@gmail.com>
2025-01-30 09:19:03 +0200

    set button connect into header image

---
+++ components/buttonConnect.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-btn
+    text="צור קשר"
+    prepend-icon="mdi-form-select"
+    aria-label="Button of google form"
+    href="https://docs.google.com/forms/d/e/1FAIpQLSf0HIU86b6gVI29v2fZb2VBrvSHVBNJ5bMo1-0vsUY7s71GXw/viewform?embedded=true"
+    target="_blank"
+    size="x-large"
+    rounded="xl"
+    color="secondary"
+  />
+</template>
--- components/headerImage.vue
+++ components/headerImage.vue
@@ -5,6 +5,7 @@
     >
       <h1 class="text-sm-h2 text-lg-h1 font-weight-thin mb-4">משה ליבוביץ</h1>
       <h2 class="text-sm-h3 text-lg-h2">מאמן רגשי לילדים ובני נוער</h2>
+      <button-connect class="mt-4" />
     </div>
   </v-parallax>
 </template>
